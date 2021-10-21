3f106d6e8cafcffa3e31a3998fb28a2f321c32bf
Ben Stephan <ben.stephan10@gmail.com>
2021-10-20 21:02:08 -0700

    eliminated static

--- budgetapp/settings.py
+++ budgetapp/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-6c372$6$4mpjwe_tg)c@*at2%e3b+d_se-f-u*nmua!b&sp&x8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://bestepha-budget-app.herokuapp.com", "localhost:8000"]
+ALLOWED_HOSTS = [
+    "https://bestepha-budget-app.herokuapp.com", "localhost:8000"
+    ]
 
 
 # Application definition
