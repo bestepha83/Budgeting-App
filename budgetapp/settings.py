c5d883855998e54d196b4524f46eec9e79410a77
Ben Stephan <ben.stephan10@gmail.com>
2021-11-13 12:11:09 -0800

    settings trial

--- budgetapp/settings.py
+++ budgetapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6c372$6$4mpjwe_tg)c@*at2%e3b+d_se-f-u*nmua!b&sp&x8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["bestepha-budget-app.herokuapp.com", "localhost", '127.0.0.1']
+ALLOWED_HOSTS = ["bestepha-budget-app.herokuapp.com", "localhost"]
 
 
 # Application definition
