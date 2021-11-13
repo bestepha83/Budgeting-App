41b77827d4bc888edf0a2d8697205b3421c0444a
Ben Stephan <ben.stephan10@gmail.com>
2021-11-12 22:17:34 -0800

    settings change

--- budgetapp/settings.py
+++ budgetapp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'budgetapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/var/www/budgetapp/budget/'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
